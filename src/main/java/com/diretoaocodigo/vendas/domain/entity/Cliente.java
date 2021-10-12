eb124f76df43eb546422718fb3f01c694e87b4b9
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-12 10:45:21 -0300

    Criação da entidade Cliente

---
+++ src/main/java/com/diretoaocodigo/vendas/domain/entity/Cliente.java
@@ -0,0 +1,31 @@
+package com.diretoaocodigo.vendas.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome", length = 100)
+    @NotEmpty(message = "{campo.nome.obrigatório}")
+    private String nome;
+
+    @Column(name = "cpf", length = 11)
+    @CPF(message="{campo.cpf.obrigatorio}")
+    @NotEmpty(message = "{campo.cpf.invalido}")
+    private String cpf;
+}
