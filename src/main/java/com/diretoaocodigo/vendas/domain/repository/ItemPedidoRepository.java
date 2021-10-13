1a2adb533c940dcd18d662024e1756d7d54c4e77
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-13 19:59:04 -0300

    Criação do repository ItemPedidoRepository

---
+++ src/main/java/com/diretoaocodigo/vendas/domain/repository/ItemPedidoRepository.java
@@ -0,0 +1,7 @@
+package com.diretoaocodigo.vendas.domain.repository;
+
+import com.diretoaocodigo.vendas.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
