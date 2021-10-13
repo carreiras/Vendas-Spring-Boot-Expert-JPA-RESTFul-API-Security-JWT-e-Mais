f7b842d86557068d68dc8530e9f92f152972c024
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-13 20:44:50 -0300

    Criação da classe PedidoServiceImpl e implementação inicial dos métodos

---
+++ src/main/java/com/diretoaocodigo/vendas/service/impl/PedidoServiceImpl.java
@@ -0,0 +1,28 @@
+package com.diretoaocodigo.vendas.service.impl;
+
+import com.diretoaocodigo.vendas.domain.entity.Pedido;
+import com.diretoaocodigo.vendas.domain.enums.StatusPedido;
+import com.diretoaocodigo.vendas.rest.dto.PeditoDTO;
+import com.diretoaocodigo.vendas.service.PedidoService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PedidoServiceImpl implements PedidoService {
+    
+    @Override
+    public Pedido save(PeditoDTO peditoDTO) {
+        return null;
+    }
+
+    @Override
+    public Optional<Pedido> bringComplete(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void updateStatus(Integer id, StatusPedido statusPedido) {
+
+    }
+}
