6f3308c2ec564fee322a088d8ae7cb5c45eee93f
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-14 09:59:44 -0300

    Implementação do método updateStatus em PedidoServiceImpl

---
+++ src/main/java/com/diretoaocodigo/vendas/exception/PedidoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.diretoaocodigo.vendas.exception;
+
+public class PedidoNaoEncontradoException extends RuntimeException {
+
+    public PedidoNaoEncontradoException() {
+        super("Pedido não encontrado.");
+    }
+}
--- src/main/java/com/diretoaocodigo/vendas/service/impl/PedidoServiceImpl.java
+++ src/main/java/com/diretoaocodigo/vendas/service/impl/PedidoServiceImpl.java
@@ -9,6 +9,7 @@ import com.diretoaocodigo.vendas.domain.repository.ClienteRepository;
 import com.diretoaocodigo.vendas.domain.repository.ItemPedidoRepository;
 import com.diretoaocodigo.vendas.domain.repository.PedidoRepository;
 import com.diretoaocodigo.vendas.domain.repository.ProdutoRepository;
+import com.diretoaocodigo.vendas.exception.PedidoNaoEncontradoException;
 import com.diretoaocodigo.vendas.exception.RegraNegocioException;
 import com.diretoaocodigo.vendas.rest.dto.ItemPedidoDTO;
 import com.diretoaocodigo.vendas.rest.dto.PedidoDTO;
@@ -16,6 +17,7 @@ import com.diretoaocodigo.vendas.service.PedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -46,8 +48,13 @@ public class PedidoServiceImpl implements PedidoService {
     }
 
     @Override
+    @Transactional
     public void updateStatus(Integer id, StatusPedido statusPedido) {
-
+        pedidoRepository.findById(id)
+                .map(pedido -> {
+                    pedido.setStatus(statusPedido);
+                    return pedidoRepository.save(pedido);
+                }).orElseThrow(() -> new PedidoNaoEncontradoException());
     }
 
     private Cliente findCliente(PedidoDTO pedidoDTO) {
