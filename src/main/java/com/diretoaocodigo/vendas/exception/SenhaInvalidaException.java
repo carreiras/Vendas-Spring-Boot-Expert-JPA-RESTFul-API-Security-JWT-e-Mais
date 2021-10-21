3631cb1efa15e3f3a3577ffff4a1e3c2bd4ea35d
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-10-21 09:15:19 -0300

    Criação da classe SenhaInvalidaException

---
+++ src/main/java/com/diretoaocodigo/vendas/exception/SenhaInvalidaException.java
@@ -0,0 +1,8 @@
+package com.diretoaocodigo.vendas.exception;
+
+public class SenhaInvalidaException extends RuntimeException {
+
+    public SenhaInvalidaException(String message) {
+        super(message);
+    }
+}
